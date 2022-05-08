37cd37152e83f9759e2c2074ab2ee3c065ee9467
araya <r1o.ryoma.abe@gmail.com>
2022-05-08 05:33:48 +0000

    Use describe function

--- test/react-typescript.test.mjs
+++ test/react-typescript.test.mjs
@@ -1,10 +1,13 @@
 import { runESLint } from "./runner.mjs";
-import { test, expect } from "vitest";
 
-test("react-typescript", async (t) => {
-  const result = await runESLint(
-    "presets/react-typescript.js",
-    "test/fixtures/react-typescript/ok.tsx"
-  );
-  expect(result.errorCount).toBe(0);
+import { describe, test, expect } from "vitest";
+
+describe("react-typescript", () => {
+  test("ok.tsx must not has any errors", async () => {
+    const result = await runESLint(
+      "presets/react-typescript.js",
+      "test/fixtures/react-typescript/ok.tsx"
+    );
+    expect(result.errorCount).toBe(0);
+  });
 });
